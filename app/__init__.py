9c3cf81915672f6e436a1b5c9e88ec753534bb1b
SkutM <ssm011403@gmail.com>
2025-10-09 18:32:22 -0700

    server working
    end of phase 1

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,8 @@ from flask import Flask
 from dotenv import load_dotenv
 
 def create_app():
-    # .env values (like SECRET_KEY) avaialbe when app starts
     load_dotenv()
 
-    # "use a separate instance/ folder as a safe home for
-    # runtime files -- and make it accessible via
-    # app.instance_path"
-    # this way, we can later do:
-    # app.config.from_pyfile("config.py", silent=True)
-    # and Flask will look inside:
-    # skutface/instance/config.py
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object("config.Config")
